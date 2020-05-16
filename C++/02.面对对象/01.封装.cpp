978b62c263a91bde14baf02f3146a1ece6f487cd
Yiming-Zuo <zuoyiming@stu.ouc.edu.cn>
2020-05-16 23:09:15 +0800

    5.16

---
+++ C++/02.面对对象/01.封装.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+	string m_name = "tom";  // 只读
+	int m_age;  // 可读可写
+	string m_lover;  // 只写
+public:
+	void get_name() {
+		cout << "name:" << m_name << endl;
+	}
+	
+	void set_age(int age) {
+		if (age < 0 || age > 100) {
+			cout << "爬！" << endl;
+			return;
+		}
+		m_age = age;
+	}
+	void get_age() {
+		cout << "age:" << m_age << endl;
+	}
+	
+	void set_lover(string lover) {
+		m_lover = lover;
+	}
+};
+
+int main(int argc, char *argv[]) {
+	Person tom;
+	tom.set_age(18);
+	tom.set_lover("貂蝉");
+	
+	tom.get_name();
+	tom.get_age();
+	return 0;
+}
